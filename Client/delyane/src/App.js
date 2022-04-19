4746c42c37334d9949094c6405e59ad6bef3d511
Delphine <delphine.pva@gmail.com>
2022-04-19 11:41:23 +0200

    Fixed error on dev

--- Client/delyane/src/App.js
+++ Client/delyane/src/App.js
@@ -2,9 +2,9 @@ import { Route, Switch } from 'react-router-dom';
 
 import React from 'react';
 
-import Footer from './components/Footer/footer';
-import Header from './components/Header/header';
-import Home from './components/Home/home';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 import './App.css';
 
