b18e2598fdf45cfcca713863abb98e2f1b58cce2
HaLan0101 <dnhalan0101@gmail.com>
2022-04-24 22:31:51 +0200

    header

--- Client/delyane/src/App.js
+++ Client/delyane/src/App.js
@@ -1,13 +1,13 @@
 import { Route, Switch } from 'react-router-dom';
-
+import './App.css';
 import React from 'react';
 
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-
-import './App.css';
-
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faBasketShopping,faUser,faHeart} from "@fortawesome/free-solid-svg-icons";
+library.add(faBasketShopping,faUser,faHeart);
 const App = () => {
   return (
     <div className="App">
