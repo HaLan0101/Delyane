43b83889f56dd2f9706c66a0531d8b3de9f78353
HaLan0101 <dnhalan0101@gmail.com>
2022-04-25 09:36:54 +0200

    add hero

--- Client/delyane/src/components/Home/Home.js
+++ Client/delyane/src/components/Home/Home.js
@@ -1,10 +1,18 @@
 import React from 'react';
-
+import './Home.css';
 const Home = () => {
     return (
-        <div>
-            <p>Hello world!</p>
+        <>
+        <div className='hero__main'>
+            <img className='hero__img' src="/hero.png" alt="" />
         </div>
+        <div className='main__bestSeller'>
+            Meilleurs Ventes
+        </div>
+        <div className='main__others'>
+            Coup de Coeur
+        </div>
+        </>
     );
 }
 
