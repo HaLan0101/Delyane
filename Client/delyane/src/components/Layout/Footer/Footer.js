f258589655ec9fd18dc141babb0fb41084dd8bbe
Delphine <delphine.pva@gmail.com>
2022-05-03 11:43:45 +0200

    Error fixed on register and authentication components, newsletter works

--- Client/delyane/src/components/Layout/Footer/Footer.js
+++ Client/delyane/src/components/Layout/Footer/Footer.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 import './Footer.css';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const Footer = () => {
+    const [newsletter, setNewsletter] = useState({});
+
+    const submitNewsletter = async (e) => {
+        console.log(newsletter);
+        e.preventDefault();
+        const url = 'http://localhost:8080/newsletter';
+        try {
+            await axios.post(url, newsletter);
+        } catch (err) {
+            console.log(err)
+        }
+    };
+
     return (
         <div className='footer__main'>
             <ul className='footer__list'>
@@ -16,18 +30,21 @@ const Footer = () => {
             </ul>
 
             <div className='footer__newsletter'>
-                <h3 className='newsletter__title'>Let's stay in touch! We'll let you know about the latest sales and new releases!</h3>
-                <div className='newsletter__content'>
-                    <input
-                        label='Email'
-                        name='email'
-                        id='email'
-                        type='mail'
-                        placeholder='Enter your email'
-                        className='newsletter__input'
-                    />
-                    <button className='newsletter__button'>Sign up</button>
-                </div>
+                <form onSubmit={(e) => submitNewsletter(e)}>
+                    <h3 className='newsletter__title'>Let's stay in touch! We'll let you know about the latest sales and new releases!</h3>
+                    <div className='newsletter__content'>
+                        <input
+                            label='Email'
+                            name='email'
+                            id='email'
+                            type='mail'
+                            placeholder='Enter your email'
+                            className='newsletter__input'
+                            onChange={(e) => setNewsletter({ ...newsletter, email: e.target.value })}
+                        />
+                        <button className='newsletter__button' type='submit'>Sign up</button>
+                    </div>
+                </form>
             </div>
 
             <div className='footer__copyright'>
